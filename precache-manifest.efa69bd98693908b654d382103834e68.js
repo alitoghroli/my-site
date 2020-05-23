e0454d51948db369d3fbbcd7a989d9091da16714
ali toghroli <alitoghroliam@gmail.com>
2020-05-23 05:22:31 -0400

    Updates

---
+++ precache-manifest.efa69bd98693908b654d382103834e68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d0af8fd26e54c28ca5f2a7d531e18d",
+    "url": "/my-site/index.html"
+  },
+  {
+    "revision": "dfa6f71ebd9297686128",
+    "url": "/my-site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44e05399626d10736e98",
+    "url": "/my-site/static/js/2.56c31db7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-site/static/js/2.56c31db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa6f71ebd9297686128",
+    "url": "/my-site/static/js/main.05300907.chunk.js"
+  },
+  {
+    "revision": "7d07631697fb56d6a253",
+    "url": "/my-site/static/js/runtime-main.9fc1db44.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
